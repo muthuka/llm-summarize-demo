ca9336ca8c52f80d6cae38d4b147068b6c69ad4a
Muthu <muthu.kumar@me.com>
2023-08-04 12:52:33 -0400

    Few adjustments to README and filenames

--- README.md
+++ README.md
@@ -16,3 +16,16 @@ Run it with
 ```sh
 python3 <filename>
 ```
+
+## Playground files
+
+| File | Purpose |
+| ---- | ------- |
+| sample-dataset.py | Loading a dataset into memory |
+| encode-decode.py | Tokenize & encode/decode |
+| trainingdata.py | Show the data used for experiment 50 & 500 |
+| zeroshot-summarize.py | Zero shot inference using "Summarize the following conversation: " |
+| zeroshot-dialogue,py | Zero shot inference using "Dialogue:" |
+| oneshot.py | One shot inference using "Dialogue:" |
+| fewshot.py | Few shots inference using "Dialogue:" |
+| fewshot-config.py | Configurable few shot inference |
---
+++ sample-dataset.py
@@ -0,0 +1,22 @@
+from datasets import load_dataset
+from transformers import AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer
+from transformers import GenerationConfig
+
+huggingface_dataset_name = "knkarthick/dialogsum"
+dataset = load_dataset(huggingface_dataset_name)
+
+example_indices = [50, 500]
+dash_line = '-'.join('' for x in range(100))
+
+for i, index in enumerate(example_indices):
+    print(dash_line)
+    print('Example ', i + 1)
+    print(dash_line)
+    print('INPUT DIALOGUE:')
+    print(dataset['test'][index]['dialogue'])
+    print(dash_line)
+    print('BASELINE HUMAN SUMMARY:')
+    print(dataset['test'][index]['summary'])
+    print(dash_line)
+    print()
